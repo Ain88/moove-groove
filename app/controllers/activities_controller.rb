6eabe93932f8af7d17578d5279a874e20a7481e1
ain88 <ahyungss@hotmail.com>
2020-05-11 17:47:44 -0700

    Fix admin email issue

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -10,11 +10,14 @@ class ActivitiesController < ApplicationController
   end
 
   def new
+    if current_user.admin != true
+      redirect_to activities_path
+    end
     @activity = Activity.new
   end
 
   def create
-    if current_user.admin != true
+    if current_user.admin != true || current_user.email != "admin@movegroove.com"
       redirect_to activities_path
     end
     @activity = Activity.new(activity_params)
@@ -27,13 +30,13 @@ class ActivitiesController < ApplicationController
   end
 
   def edit
-    if current_user.admin != true
+    if current_user.admin != true || current_user.email != "admin@movegroove.com"
       redirect_to activities_path
     end
   end
 
   def update
-    if current_user.admin != true
+    if current_user.admin != true || current_user.email != "admin@movegroove.com"
       redirect_to activities_path
     end
     if @activity.update(activity_params)
